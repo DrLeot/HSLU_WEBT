1399bb3c03d3a3b76d6b6fb3933d05e018c0f727
Janik Burkart <janik.burkart@bluewin.ch>
2021-11-30 14:19:11 +0100

    Errorhandling in PHP backend

--- 0_Modulnachweis/UpsertMenu.php
+++ 0_Modulnachweis/UpsertMenu.php
@@ -1,55 +1,96 @@
 <?php
-$menuname = $_POST['menuname'];
-$ingredients = $_POST['ingredients'];
-$category = $_POST['category'];
-$servesfor = intval($_POST['amountpeople']);
-
-// build up connection
-$connection = new mysqli("localhost","root","", "menus");
-if($connection->connect_error){
-    die("Connection failed: " . $connection->connect_error);
+function printSuccess($msg){
+    echo "<p style='color:green;'>Success: ".$msg."</p>";
+}
+function printWarning($msg){
+    echo "<p style='color:yellow;'>Warning: " .$msg."</p>";
+}
+function printError($msg){
+    die("<p style='color:red;'>Error: " .$msg."</p>");
 }
 
-// check if menu already exists
-$sql_menu_exists = $connection->prepare("SELECT menu_name FROM menu WHERE menu_name=?");
-$sql_menu_exists->bind_param("s", $menuname);
-$sql_menu_exists->execute();
-$sql_menu_exists->store_result();
-$number_of_existing = $sql_menu_exists->num_rows;
-$sql_menu_exists->close();
+function connectDB(){
+    $connection = new mysqli("localhost","root","", "menus");
+    if($connection->connect_error){
+        printError($connection->connect_error);
+    }
+    return $connection;
+}
+function closeDB($conn){
+    $conn->close();
+}
 
+function upsertMenuData($connection, $values){
 
-if($number_of_existing > 0){
-    // the menu already exists: update ingredients
-    $sql_menu_update = $connection->prepare("UPDATE menu SET menu_category=?,menu_ingredients=?,menu_servesfor=? WHERE menu_name=?");
-    $sql_menu_update->bind_param("ssis", $category, $ingredients, $servesfor, $menuname);
-    if(!$sql_menu_update->execute()){
-        die("Das gewünschte Menü konnte nicht aktualisiert werden.");
+    $ret = false;
+    foreach($values as $key=> $value){
+        if(!isset($value) or empty($value)){
+            printWarning($key." seems not to be set. Data to DB will not be imported or updated.");
+            $ret = true;
+        }
+    }
+    if($ret){
+        return;
     }
 
-}else{
-    // the menu doesn't exists: insert
-    $sql_menu_insert = $connection->prepare("INSERT INTO menu VALUES (?, ?, ?, ?)");
-    if(!$sql_menu_insert){
-        die($connection->error);
-    }
+    // check if menu already exists
+    $sql_menu_exists = $connection->prepare("SELECT menu_name FROM menu WHERE menu_name=?");
+    $sql_menu_exists->bind_param("s", $values["menuname"]);
+    $sql_menu_exists->execute();
+    $sql_menu_exists->store_result();
+    $number_of_existing = $sql_menu_exists->num_rows;
+    $sql_menu_exists->close();
 
-    $sql_menu_insert->bind_param("sssi", $menuname, $category, $ingredients, $servesfor);
-    if(!$sql_menu_insert->execute()){
-        die("Das gewünschte Menü konnte nicht eingefügt werden.");
+
+    if($number_of_existing > 0){
+        // the menu already exists: update ingredients
+        $sql_menu_update = $connection->prepare("UPDATE menu SET menu_category=?,menu_ingredients=?,menu_servesfor=? WHERE menu_name=?");
+        $sql_menu_update->bind_param("ssis", $values["category"], $values["ingredients"], $values["servesfor"], $values["menuname"]);
+        if(!$sql_menu_update->execute()){
+            printError("Das gewünschte Menü konnte nicht aktualisiert werden.");
+        }
+        printSuccess("Das Menü ".$values["menuname"]." wurde erfolgreich aktualisiert.");
+
+    }else{
+        // the menu doesn't exists: insert
+        $sql_menu_insert = $connection->prepare("INSERT INTO menu VALUES (?, ?, ?, ?)");
+        if(!$sql_menu_insert){
+            printError($connection->error);
+        }
+
+        $sql_menu_insert->bind_param("sssi", $values["menuname"], $values["category"], $values["ingredients"], $values["servesfor"]);
+        if(!$sql_menu_insert->execute()){
+            printError("Das gewünschte Menü konnte nicht eingefügt werden.");
+        }
+        printSuccess("Das Menü ".$values["menuname"]." wurde erfolgreich eingefügt.");
     }
 }
 
-// show all maintained menus
-$sql_menu_all = $connection->prepare("SELECT * FROM menu;");
-$sql_menu_all->execute();
-$result = $sql_menu_all->get_result();
-while ($row = $result->fetch_assoc()) {
-    echo "<h1>".$row['menu_name']."</h1>";
-    echo "<h5> Berechnet für ".$row['menu_servesfor']." Personen als ".$row['menu_category']."</h5>";
-    echo "<p>".$row['menu_ingredients']."</p></br>";
+function printAllMenus($connection){
+    // show all maintained menus
+    $sql_menu_all = $connection->prepare("SELECT * FROM menu order by menu_name ASC;");
+    $sql_menu_all->execute();
+    $result = $sql_menu_all->get_result();
+    while ($row = $result->fetch_assoc()) {
+        echo "<h1>".$row['menu_name']."</h1>";
+        echo "<h5> Berechnet für ".$row['menu_servesfor']." Personen als ".$row['menu_category']."</h5>";
+        echo "<p>".$row['menu_ingredients']."</p></br>";
 
+    }
 }
 
-$connection->close();
+$DBConnection = connectDB();
+echo upsertMenuData(
+    $DBConnection,
+    array(
+        "menuname" => $_POST['menuname'],
+        "ingredients" => $_POST['ingredients'],
+        "category" => $_POST['category'],
+        "servesfor" => intval($_POST['amountpeople'])
+    )
+);
+echo "<a href='index.html'>Zurück zur Webseite</a>";
+printAllMenus($DBConnection);
+closeDB($DBConnection);
+
 ?>
